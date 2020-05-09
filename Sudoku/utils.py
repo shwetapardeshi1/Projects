70ea2677a57122a8c521f67fd45f3d67e4959a8e
shwetapardeshi1 <51332741+shwetapardeshi1@users.noreply.github.com>
2020-05-09 18:40:17 +0530

    Create utils.py

---
+++ Sudoku/utils.py
@@ -0,0 +1,103 @@
+import numpy as np
+import cv2
+import operator
+from matplotlib import pyplot as plt
+
+def show_digits(digits, colour=255):
+    rows = []
+    with_border = [cv2.copyMakeBorder(img.copy(), 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, colour) for img in digits]
+    for i in range(9):
+        row = np.concatenate(with_border[i * 9:((i + 1) * 9)], axis=1)
+        rows.append(row)
+        img = np.concatenate(rows)
+    return img
+    
+def scale_and_centre(img, size, margin=0, background=0):
+    h, w = img.shape[:2]
+    if h > w:
+      t_pad = int(margin / 2)
+      b_pad = t_pad
+      ratio = (size - margin) / h
+      w, h = scale(ratio, w), scale(ratio, h)
+      l_pad, r_pad = centre_pad(w)
+    else:
+      l_pad = int(margin / 2)
+      r_pad = l_pad
+      ratio = (size - margin) / w
+      w, h = scale(ratio, w), scale(ratio, h)
+      t_pad, b_pad = centre_pad(h)
+
+    img = cv2.resize(img, (w, h))
+    img = cv2.copyMakeBorder(img, t_pad, b_pad, l_pad, r_pad, cv2.BORDER_CONSTANT, None, background)
+    return cv2.resize(img, (size, size))
+    
+def extract_digit(img, rect, size):
+
+
+    digit = img[int(rect[0][1]):int(rect[1][1]), int(rect[0][0]):int(rect[1][0])]  # Get the digit box from the whole square
+
+
+    h, w = digit.shape[:2]
+    margin = int(np.mean([h, w]) / 2.5)
+    _, bbox, seed = find_largest_feature(digit, [margin, margin], [w - margin, h - margin])
+    digit = digit[int(bbox[0][1]):int(bbox[1][1]), int(bbox[0][0]):int(bbox[1][0])]
+
+    w = bbox[1][0] - bbox[0][0]
+    h = bbox[1][1] - bbox[0][1]
+
+    if w > 0 and h > 0 and (w * h) > 100 and len(digit) > 0:
+      return scale_and_centre(digit, size, 4)
+    else:
+      return np.zeros((size, size), np.uint8)
+      
+      
+ def find_largest_feature(inp_img, scan_tl=None, scan_br=None):
+
+    img = inp_img.copy()  
+    height, width = img.shape[:2]
+
+    max_area = 0
+    seed_point = (None, None)
+
+    if scan_tl is None:
+      scan_tl = [0, 0]
+
+    if scan_br is None:
+      scan_br = [width, height]
+
+    for x in range(scan_tl[0], scan_br[0]):
+      for y in range(scan_tl[1], scan_br[1]):
+        # Only operate on light or white squares
+        if img.item(y, x) == 255 and x < width and y < height:  # Note that .item() appears to take input as y, x
+          area = cv2.floodFill(img, None, (x, y), 64)
+          if area[0] > max_area:  # Gets the maximum bound area which should be the grid
+            max_area = area[0]
+            seed_point = (x, y)
+
+
+    for x in range(width):
+      for y in range(height):
+        if img.item(y, x) == 255 and x < width and y < height:
+          cv2.floodFill(img, None, (x, y), 64)
+
+    mask = np.zeros((height + 2, width + 2), np.uint8)  # Mask that is 2 pixels bigger than the image
+
+
+    if all([p is not None for p in seed_point]):
+      cv2.floodFill(img, mask, seed_point, 255)
+
+    top, bottom, left, right = height, 0, width, 0
+
+    for x in range(width):
+      for y in range(height):
+        if img.item(y, x) == 64:  # Hide anything that isn't the main feature
+          cv2.floodFill(img, mask, (x, y), 0)
+
+        if img.item(y, x) == 255:
+          top = y if y < top else top
+          bottom = y if y > bottom else bottom
+          left = x if x < left else left
+          right = x if x > right else right
+
+    bbox = [[left, top], [right, bottom]]
+    return img, np.array(bbox, dtype='float32'), seed_point
